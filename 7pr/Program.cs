cf7fc9a3239be33e4b624949d3030f3ee9bd940f
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-03-10 19:48:41 +0200

    Add files via upload

---
+++ 7pr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _7pr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Composition> compositions = new List<Composition>();
+            compositions.Add(new Composition("Корм", 10, 150));
+            compositions.Add(new Composition("Ошейник", 20, 250));
+            foreach(var elem in compositions)
+            {
+                WriteLine();
+                Write(elem.Info());
+            }
+            ReadKey();
+        }
+    }
+    
+}
---
+++ 7pr/Сomposition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _7pr
+{
+    class Composition
+    {
+        public string Product { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+        public Composition() { }
+        public Composition(string product, int count, double price)
+        {
+            Product = product;
+            Count = count;
+            Price = price;
+        }
+        public double Prices()
+        {
+            return Count * Price;
+        }
+        public string Info()
+        {
+            return $"Продукт: {Product} Количество на складе:{Count} Цена за штуку:{Price} Общая стоимость:{Prices()}";
+        }
+    }
+}
