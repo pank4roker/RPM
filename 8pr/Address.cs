c899d9eb499094d14ec9505001455ca6da8f53a9
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-03-11 09:50:44 +0200

    Add files via upload

---
+++ 8pr/Address.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _8pr
+{
+    class Address
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int NumberFlat { get; set; }
+        public int NumberPhone { get; set; }
+        public string Name {  get; set; }
+        public string Surname { get; set; }
+        public string Otch { get; set; }
+        public Address() { }
+        public Address(string city, string street, int numberFlat, int numberPhone, string name,string surname, string otch)
+        {
+            City = city;
+            Street = street;
+            NumberFlat = numberFlat;
+            NumberPhone = numberPhone;
+            Name = name;
+            Surname = surname;
+            Otch = otch;
+        }
+        public void Info()
+        {
+            WriteLine($"{City} {Street} {NumberFlat} {NumberPhone} {Name} {Surname} {Otch}");
+        }
+        public void Vibor(string dan)
+        {
+            if (Name == dan )
+            {
+                WriteLine($"{City}{Street}{NumberFlat}");
+            }
+            else if(Surname == dan)
+            {
+                WriteLine($"{City}{Street}{NumberFlat}");
+            }
+            else if (City == dan && Street == dan && Convert.ToString(NumberFlat) == dan)
+            {
+                WriteLine($"{Name} {Surname} {Otch}");
+            }
+            else 
+            {
+                WriteLine($"{City}{Street}{NumberFlat}{Name} {Surname} {Otch}");
+            }
+        }
+
+    }
+}
---
+++ 8pr/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _8pr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Address> addresses = new List<Address>();
+            addresses.Add(new Address("Paris","Петровская",34,77520915,"Kirill","Markow","Alecs"));
+            foreach (Address address in addresses) 
+            {
+                address.Info();
+            }
+            Write("Введите Адрес/Фио/Адрес или ФИО: ");
+            string dan = ReadLine();
+            foreach (Address address in addresses) 
+            {
+                address.Vibor(dan);
+            }
+            ReadKey();
+        }
+    }
+}
